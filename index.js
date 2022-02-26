0751b796222fc5494318234f2a443ae902629a5e
akhhanna20 <akhhanna20@gmail.com>
2022-02-26 15:37:47 -0500

    routes

--- index.js
+++ index.js
@@ -1,47 +1,12 @@
 const express = require('express');
-let bodyParser = require('body-parser');
+const routes = require("./routes");
+const bodyParser = require("./bodyParser");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-app.use(bodyParser.json());
-
-let cards = [
-    {id: '1', name: 'First card', status: 'todo', priority: 2},
-    {id: '2', name: 'Second card', status: 'progress', priority: 5}
-];
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
-
-app.get('/cards', (req, res) => {
-    res.send(cards);
-});
-
-app.delete('/cards/:cardId', (req, res) => {
-    console.log(req)
-    const cardId = req.params.cardId;
-    cards = cards.filter(el => el.id !== cardId);
-    res.send(cards);
-})
-
-app.post('/cards', (req, res) => {
-    console.log(req)
-    const card = req.body;
-    cards.push({id: Math.random().toString(), ...card});
-    res.send('Card created');
-})
-app.patch('/cards/:cardId', (req, res) => {
-    const cardId = req.params.cardId;
-    const card = req.body;
-    cards = cards.map(el =>
-    el.id === cardId ? ({...card, id: el.id}) : el)
-    res.send('Card updated');
-})
+bodyParser();
+routes(app);
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
---
+++ routes.js
@@ -0,0 +1,36 @@
+let cards = [
+    {id: '1', name: 'First card', status: 'todo', priority: 2},
+    {id: '2', name: 'Second card', status: 'progress', priority: 5}
+];
+function routes(app) {
+
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+    });
+
+    app.get('/cards', (req, res) => {
+        res.send(cards);
+    });
+
+    app.delete('/cards/:cardId', (req, res) => {
+        console.log(req)
+        const cardId = req.params.cardId;
+        cards = cards.filter(el => el.id !== cardId);
+        res.send(cards);
+    });
+
+    app.post('/cards', (req, res) => {
+        console.log(req)
+        const card = req.body;
+        cards.push({id: Math.random().toString(), ...card});
+        res.send('Card created');
+    })
+    app.patch('/cards/:cardId', (req, res) => {
+        const cardId = req.params.cardId;
+        const card = req.body;
+        cards = cards.map(el =>
+            el.id === cardId ? ({...card, id: el.id}) : el)
+        res.send('Card updated');
+    })
+}
+module.exports = routes;
